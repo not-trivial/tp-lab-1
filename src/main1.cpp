3b4c3a336eefc71aedaf39f37be83ee352c2006f
unknown <aisidneva@edu.hse.com>
2018-12-09 01:57:27 +0300

    update task1

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include "task1.h"
 using namespace std;
 
--- src/task1.cpp
+++ src/task1.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <algorithm>
 #include "task1.h"
 
 unsigned long gcd(unsigned int a, unsigned int b) { //finding great common divisor
